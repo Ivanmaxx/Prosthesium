5f71ef2bc68b715969160b2255d8b624e1e42d4a
Иван Левков <ai.salome@yahoo.com>
2024-10-25 13:49:57 +0300

    WIP fix

--- Godot/Prosthesium/Player.gd
+++ Godot/Prosthesium/Player.gd
@@ -8,24 +8,27 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var can_attack = true
 var hitbox = null
 var if_has_key = false
-#var jump_max = 1
-#var jump_cnt = 0
 @onready var _animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	#if player_states.prostheses["leg"] == "basic_prosth": 
-		#jump_max = 2
+		#player_states.jump_max = 2
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	if is_on_floor(): #jump_cnt < jump_max
-		if Input.is_action_just_pressed("ui_accept"):
-			jump()
-			#if is_on_floor():
-				#print("daun")
-	if Input.is_action_just_released("ui_accept"):	
+	if Input.is_action_just_pressed("ui_accept"):
+		#player_states.skibidi += 1
+		#print(player_states.skibidi)
+		#print(str(player_states.skibidi) + " " + str(player_states.jump_cnt) + " " + str(player_states.jump_max) + str(bool(player_states.jump_cnt < player_states.jump_max)))
+		#if player_states.jump_cnt < player_states.jump_max:
+			#player_states.jump_cnt = player_states.skibidi
+			#print(str(player_states.jump_cnt) + " " + str(player_states.jump_max) + " " + str(player_states.skibidi))
+		jump()
+	if Input.is_action_just_released("ui_accept"):
 		jump_cut()
-	#if is_on_floor:
-		#jump_cnt = 0
+	#if is_on_floor():
+		#player_states.jump_cnt = 0
+		#print(player_states.jump_cnt)
+
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -SPEED
 		_animated_sprite.play("run")
@@ -34,6 +37,7 @@ func _physics_process(delta):
 		velocity.x = SPEED
 		_animated_sprite.flip_h = false
 		_animated_sprite.play("run")
+
 	if Input.is_action_pressed("hand_action_1") and can_attack:
 		_animated_sprite.play("punch")
 		can_attack = false
@@ -56,6 +60,5 @@ func jump():
 	velocity.y = -JUMP_VELOCITY
 	
 func jump_cut():
-	#jump_cnt += 1
 	if velocity.y < -150:
 		velocity.y = -150
--- Godot/Prosthesium/player_states.gd
+++ Godot/Prosthesium/player_states.gd
@@ -1,6 +1,9 @@
 extends Node
 
 var prostheses = {"head": "", "torso": "", "left_arm": "", "right_arm": "", "leg": ""}
+var skibidi = 0
+var jump_cnt = 0
+var jump_max = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
